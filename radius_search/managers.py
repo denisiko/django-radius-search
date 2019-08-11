e7453dc26d6066d2e2846d812161ddbc06590006
denisiko <denis@anuschewski.com>
2019-08-11 05:10:52 +0200

    Control distance unit via query set function or manager function

--- radius_search/managers.py
+++ radius_search/managers.py
@@ -1,20 +1,10 @@
 from django.db.models.manager import BaseManager
 
 from .querysets import LocationQuerySet
-from .settings import DISTANCE_UNIT_KM
 
 
 class LocationManager(BaseManager.from_queryset(LocationQuerySet)):
     """
     Manager class for location models constructed from LocationQuerySet.
-    Overwrite with DISTANCE_UNIT as DISTANCE_UNIT_MILES to customize.
+    Overwrite get_distance_unit for customization.
     """
-    DISTANCE_UNIT = DISTANCE_UNIT_KM
-
-    def get_queryset(self):
-        """
-        Returns the instantiated query set with DISTANCE_UNIT passed into it.
-        :return: query set for locations
-        """
-        return self._queryset_class(model=self.model, using=self._db, hints=self._hints,
-                                    distance_unit=self.DISTANCE_UNIT)
--- radius_search/querysets.py
+++ radius_search/querysets.py
@@ -14,27 +14,18 @@ class LocationQuerySet(models.QuerySet):
     """
     Query set class for location models.
     """
-    def __init__(self, distance_unit=DISTANCE_UNIT_KM, *args, **kwargs):
-        """
-        Initializes the query set instance and sets distance_unit.
-        :param distance_unit: 'km' (default) or 'mi'
-        :param args: other arguments
-        :param kwargs: other arguments as keywords
-        """
-        self.distance_unit = distance_unit
-        super(LocationQuerySet, self).__init__(*args, **kwargs)
-
     def perimeter(self, mid_point, radius, latitude='latitude', longitude='longitude'):
         """
         Returns a query set of locations in a specified radius (using the Haversine formula).
         :param mid_point: middle point coordinates of search radius (e.g. tuple of floats)
-        :param radius: search radius in km (default) or miles depending on distance_unit
+        :param radius: search radius in km (default) or miles depending on get_distance_unit
         :param latitude: query selector for latitude
         :param longitude: query selector for longitude
         :return: Annotated query set of found locations
         """
+        earth_radius = dict(EARTH_RADIUS_CHOICES).get(self.get_distance_unit())
         distance = (
-            self.get_earth_radius()
+            earth_radius
             * ACos(
                 Cos(Radians(latitude))
                 * Cos(Radians(mid_point[0]))
@@ -45,9 +36,11 @@ class LocationQuerySet(models.QuerySet):
         )
         return self.annotate(distance=distance).filter(distance__lte=radius)
 
-    def get_earth_radius(self):
+    @staticmethod
+    def get_distance_unit():
         """
-        Returns the earth radius in km (default) or miles depending on distance_unit.
-        :return: Integer value for earth radius
+        Returns the distance unit for search radius results.
+        Valid choices are 'km' (default) and 'mi'. Overwrite for customization.
+        :return: string value for distance unit
         """
-        return dict(EARTH_RADIUS_CHOICES).get(self.distance_unit)
+        return DISTANCE_UNIT_KM
