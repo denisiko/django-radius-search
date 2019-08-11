39590b4de6a9061f6a5aa658f14351ccfd13070b
denisiko <denis@anuschewski.com>
2019-08-11 04:31:57 +0200

    Add back get_queryset() in LocationManager

--- radius_search/managers.py
+++ radius_search/managers.py
@@ -7,6 +7,15 @@ from .settings import DISTANCE_UNIT_KM
 class LocationManager(BaseManager.from_queryset(LocationQuerySet)):
     """
     Manager class for location models constructed from LocationQuerySet.
-    Overwrite with DISTANCE_UNIT as DISTANCE_UNIT_MILES to customise.
+    Overwrite with DISTANCE_UNIT as DISTANCE_UNIT_MILES to customize.
     """
     DISTANCE_UNIT = DISTANCE_UNIT_KM
+
+    def get_queryset(self):
+        """
+        Returns the query set with DISTANCE_UNIT passed into it.
+        :return: query set for locations
+        """
+        queryset = super(LocationManager, self).get_queryset()
+        queryset.distance_unit = self.DISTANCE_UNIT
+        return queryset
